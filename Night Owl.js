94c40625dd7abe6199f8570e791ea42301032dde
OvertBlue <81298874+OvertBlue@users.noreply.github.com>
2021-03-24 20:23:05 +0900

    Create Night Owl.js

---
+++ Night Owl.js
@@ -0,0 +1,21 @@
+black       = '#011627';
+blue        = '#82AAFF'; // blue
+cyan        = '#21C7A8'; // cyan
+green       = '#22DA6E'; // green
+magenta     = '#C792EA'; // pink
+red         = '#EF5350'; // red
+white       = '#ffffff'; // light gray
+yellow      = '#ADDB67'; // yellow
+lightBlack  = '#969696'; // medium gray
+lightBlue   = '#82AAFF'; // blue
+lightCyan   = '#7FDBCA'; // cyan
+lightGreen  = '#22DA6E'; // green
+lightMagenta= '#C792EA'; // pink
+lightRed    = '#EF5350'; // red
+lightWhite  = '#E0E0E0'; // white
+lightYellow = '#FFEB95'; // yellow
+
+t.prefs_.set('color-palette-overrides',[ black, red, green, yellow, blue, magenta, cyan, white, lightBlack, lightRed, lightGreen, lightYellow, lightBlue, lightMagenta, lightCyan, lightWhite ]);
+t.prefs_.set('cursor-color', 'rgba(128, 164, 194, 0.5)');
+t.prefs_.set('foreground-color', '#D6DEEB');
+t.prefs_.set('background-color', black);
