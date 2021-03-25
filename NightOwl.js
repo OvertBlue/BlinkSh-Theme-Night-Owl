72c9a482227fac5d6dd8bd18c7f3de71699b5a4e
OvertBlue <81298874+OvertBlue@users.noreply.github.com>
2021-03-25 09:12:30 +0900

    Remove variables from NightOwl.js
    Remove variables for consistency with other BlinkSh themes

--- NightOwl.js
+++ NightOwl.js
@@ -1,21 +1,4 @@
-black       = '#011627';
-blue        = '#82AAFF'; // blue
-cyan        = '#21C7A8'; // cyan
-green       = '#22DA6E'; // green
-magenta     = '#C792EA'; // pink
-red         = '#EF5350'; // red
-white       = '#ffffff'; // light gray
-yellow      = '#ADDB67'; // yellow
-lightBlack  = '#969696'; // medium gray
-lightBlue   = '#82AAFF'; // blue
-lightCyan   = '#7FDBCA'; // cyan
-lightGreen  = '#22DA6E'; // green
-lightMagenta= '#C792EA'; // pink
-lightRed    = '#EF5350'; // red
-lightWhite  = '#E0E0E0'; // white
-lightYellow = '#FFEB95'; // yellow
-
-t.prefs_.set('color-palette-overrides',[ black, red, green, yellow, blue, magenta, cyan, white, lightBlack, lightRed, lightGreen, lightYellow, lightBlue, lightMagenta, lightCyan, lightWhite ]);
+t.prefs_.set('color-palette-overrides',['#011627', '#EF5350', '#22DA6E', '#ADDB67', '#82AAFF', '#C792EA', '#21C7A8', '#ffffff', '#969696', '#EF5350', '#22DA6E', '#FFEB95', '#82AAFF', '#C792EA', '#7FDBCA', '#E0E0E0']);
 t.prefs_.set('cursor-color', 'rgba(128, 164, 194, 0.5)');
 t.prefs_.set('foreground-color', '#D6DEEB');
-t.prefs_.set('background-color', black);
+t.prefs_.set('background-color', '#011627');
