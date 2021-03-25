dc939acf4f8f07f554b9e009c2e14e0f67a920a2
OvertBlue <trenton@merren.us>
2021-03-25 10:59:01 +0900

    Correct color values

--- NightOwl.js
+++ NightOwl.js
@@ -1,4 +1,4 @@
-t.prefs_.set('color-palette-overrides',['#011627', '#EF5350', '#22DA6E', '#ADDB67', '#82AAFF', '#C792EA', '#21C7A8', '#ffffff', '#969696', '#EF5350', '#22DA6E', '#FFEB95', '#82AAFF', '#C792EA', '#7FDBCA', '#E0E0E0']);
-t.prefs_.set('cursor-color', 'rgba(128, 164, 194, 0.5)');
+t.prefs_.set('color-palette-overrides',['#011627', '#EF5350', '#22DA6E', '#C5E478', '#82AAFF', '#C792EA', '#21C7A8', '#E0E0E0', '#575656', '#EF5350', '#22DA6E', '#FFEB95', '#82AAFF', '#C792EA', '#7FDBCA', '#FFFFFF']);
+t.prefs_.set('cursor-color', 'rgba(35, 77, 112, 0.5)');
 t.prefs_.set('foreground-color', '#D6DEEB');
 t.prefs_.set('background-color', '#011627');
